82e429f7d3686adf6c39eaf29ab186f490fdc35b
ljkeller <kellerjlucas@gmail.com>
2023-02-22 17:47:01 -0600

    Navigation pane now opens/closes
    Upon menu icon click, navigation pane opens and closes. New index.js
    file.

---
+++ public/index.js
@@ -0,0 +1,6 @@
+let menuicon = document.querySelector(".menuicon");
+let nav = document.querySelector(".nav-container");
+
+
+// closes nav pane on button click
+menuicon.addEventListener("click", ()=> {nav.classList.toggle("navclose")})
