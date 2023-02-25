d29480bb4890d51e07d36a15d50ddb4f4d7bbc9a
ljkeller <kellerjlucas@gmail.com>
2023-02-25 14:32:25 -0600

    Styling Updates (color and formatting)
    For the asset container:
    Columns are now aligned, and colors are now dynamically applied based
    off of state values.

--- public/index.js
+++ public/index.js
@@ -4,3 +4,30 @@ let nav = document.querySelector(".nav-container");
 
 // closes nav pane on button click
 menuicon.addEventListener("click", ()=> {nav.classList.toggle("navclose")})
+
+const stateCollection = document.getElementsByClassName("state-tag");
+for (let element of stateCollection) {
+    const state = element.textContent;
+    if (state && state.toLowerCase() === "off") {
+        element.style.backgroundColor = getComputedStyle(document.documentElement)
+            .getPropertyValue('--tertiary-color');
+    } else if (state && state.toLowerCase() === "maintenance") {
+        element.style.backgroundColor = getComputedStyle(document.documentElement)
+            .getPropertyValue('--secondary-color-tetr1');
+    } else {
+        element.style.backgroundColor = getComputedStyle(document.documentElement)
+            .getPropertyValue('--healthy-color');
+    }
+}
+
+const availibilityCollection = document.getElementsByClassName("availibility-tag");
+for (let element of availibilityCollection) {
+    const availibility = element.textContent;
+    if (availibility && availibility.toLowerCase() === "claimed") {
+        element.style.backgroundColor = getComputedStyle(document.documentElement)
+            .getPropertyValue('--secondary-color-tetr1');
+    } else {
+        element.style.backgroundColor = getComputedStyle(document.documentElement)
+            .getPropertyValue('--healthy-color');
+    }
+}
