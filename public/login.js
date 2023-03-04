74c8b08f9899999d979c3e851c54802bfc81bae1
ljkeller <kellerjlucas@gmail.com>
2023-03-04 16:52:43 -0600

    Post request for login page on 'submit'
    Now, when the user clicks 'submit' on the /login page, the form data is
    extracted and built into a POST request. Login with that to gain JWT.
    
    TODO: use that JWT for auth, block requests without JWT intelligently.
    
    TOOD: sign up page POST request.

--- public/login.js
+++ public/login.js
@@ -3,10 +3,36 @@ console.log(form);
 form.addEventListener('submit', (event) => {
     // dont let user spam empty boxes
     event.preventDefault();
+    
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let payload = JSON.stringify({
+        email : document.getElementById('email').value,
+        password : document.getElementById('password').value
+    });
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    (async () => {
+        try {
+            const res = await fetch('/user/login', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: payload
+            });
 
-    // POST here
+            if (!res.ok) { throw new Error('Failure to login: '.concat(res.status));};
+            tok = await res.json();
+            location.href='/';
+
+            // TODO: embedd JWT into auth bearer?
+        } catch (error) {
+            console.error(error);
+            alert('Failed to login');
+        }
+    })();
+     
+
     console.log(`email: ${email} pass: ${password}`);
 });
