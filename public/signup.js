99f3043f9482faa3c0b03c88789adc0fe695e241
ljkeller <kellerjlucas@gmail.com>
2023-03-04 17:25:44 -0600

    New signup POST
    Clicking 'submit' now POSTS the user's form to mongoDB for account
    creation, as long as the inputs are valid. The result of the fetch is
    a JWT.
    
    TOOD: use the JWT for other endpoints.

--- public/signup.js
+++ public/signup.js
@@ -1,13 +1,35 @@
 const form = document.querySelector('.sign-up-box');
-console.log(form);
 form.addEventListener('submit', (event) => {
     // dont let user spam empty boxes
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    let payload = JSON.stringify({
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    });
 
-    // POST here
-    console.log(`username: ${username} email: ${email} pass: ${password}`);
+    (async () => {
+        try {
+            let res = await fetch('/user/signup', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/JSON',
+                    'Content-Type': 'application/JSON'
+                },
+                body: payload
+            });
+
+            if (!res.ok) {throw new Error('Failure to create account: '.concat((await res).status));}
+
+            const jwt = await res.json();
+            location.href = '/';
+            // TODO: embedd JWT into auth bearer?
+        } catch (error) {
+            console.error(error)
+            alert('Failed to create account');
+        }
+    })();
+
+    console.log(`Payload: ${payload}`);
 });
